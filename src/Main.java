82a1058092f0d9f5414d464bfe64bb162e87f236
nowaknat <natalia.nowak@tieto.com>
2018-11-19 15:30:15 +0100

    dodalam zadanie4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 
+import java.util.Date;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,9 +10,23 @@ public class Main {
 
         System.out.println("Hello World!");
 
-    //ZADANIE 2
+        //ZADANIE 2
 
         System.out.println("Natalia 23");
+
+
+        //ZADANIE 4
+
+        Scanner skaner_tak_cie_nazwalam = new Scanner(System.in);
+        System.out.println("Podaj pierwsza liczbe");
+        double liczba1 = skaner_tak_cie_nazwalam.nextDouble();
+        System.out.println("Podaj druga liczbe");
+        double liczba2 = skaner_tak_cie_nazwalam.nextDouble();
+        System.out.println(liczba1 + liczba2);
+        System.out.println(liczba1 - liczba2);
+        System.out.println(liczba1 * liczba2);
+
+
 
 
 
