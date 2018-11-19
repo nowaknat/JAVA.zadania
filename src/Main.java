9bcaae878ae1bc5c7f520b3501844ea22b548889
nowaknat <natalia.nowak@tieto.com>
2018-11-19 15:02:19 +0100

    dodalam zadanie 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 public class Main {
 
+    public static void main(String[] args) {
+        //ZADANIE 1
 
-    public static void main(String[] args)
-    //ZADANIE 1
-    {
         System.out.println("Hello World!");
-    }
 
+    //ZADANIE 2
+
+        System.out.println("Natalia 23");
+
+
+
+
+
+    }
 }
