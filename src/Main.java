ce3de7b5bdb1823bf80580bf2aa577e3bd121fe9
nowaknat <natalia.nowak@tieto.com>
2018-11-19 14:51:55 +0100

    dodalam zadanie 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args)
+    //ZADANIE 1
+    {
         System.out.println("Hello World!");
     }
+
 }
