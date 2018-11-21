961ad3438bdd9c60451b71d62d105e099e2d71fa
nowaknat <natalia.nowak@tieto.com>
2018-11-21 15:21:11 +0100

    dodałam zadanie 5 i 7

--- src/JAVA2/Main2.java
+++ src/JAVA2/Main2.java
@@ -7,7 +7,8 @@ public class Main2 {
 
         //ZADANIE1
         Scanner skanerMoj = new Scanner(System.in);
-        System.out.println("Podaj liczbe całkowita");
+        System.out.println("ZADANIE 1:");
+        System.out.println("Podaj liczbę całkowitą");
         int liczba = skanerMoj.nextInt();
            if (liczba % 2 == 0) {
            System.out.println("twoja liczba jest parzysta");
@@ -17,7 +18,8 @@ public class Main2 {
 
 
         //ZADANIE2
-        System.out.println("Podaj liczbe całkowita");
+        System.out.println("ZADANIE 2:");
+        System.out.println("Podaj liczbę całkowitą");
         int liczba2 = skanerMoj.nextInt();
         int a = liczba2 % 2;
         switch (a) {
@@ -30,7 +32,8 @@ public class Main2 {
         }
 
         //ZADANIE5
-        System.out.println("Podaj liczbe całkowita");
+        System.out.println("ZADANIE 5:");
+        System.out.println("Podaj liczbę całkowitą");
         int liczba3 = skanerMoj.nextInt();
         if (liczba3 >= 1 && liczba3 <=10){
             System.out.println("twoja liczba należy do przedziału <1,10>");}
@@ -38,6 +41,26 @@ public class Main2 {
             System.out.println("twoja liczba nie należy do przedziału <1,10>");}
 
 
+//        //ZADANIE6
+//          System.out.println("ZADANIE 6:");
+//        do { System.out.println("Podaj liczbę całkowitą");
+//            int liczba4 = skanerMoj.nextInt()}
+//        liczba4 = int b;
+//        while (b !=0 && b !=5);
+
+
+        //ZADANIE7
+        System.out.println("ZADANIE 7:");
+        System.out.println("Podaj liczbę całkowitą");
+        int liczba5 = skanerMoj.nextInt();
+        while (liczba5 %2 == 0){
+            System.out.println("twoja liczba jest parzysta");
+            break;
+        }
+        while (liczba5 %2 == 1){
+            System.out.println("twoja liczba jest nieparzysta");
+            break;
+        }
 
     }
 }
