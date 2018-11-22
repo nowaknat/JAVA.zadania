d87bf0281688f8265d27bcd098d380e4e14e18f7
nowaknat <natalia.nowak@tieto.com>
2018-11-22 12:16:09 +0100

    dodałam zadanie 3

--- src/JAVA2/Main2.java
+++ src/JAVA2/Main2.java
@@ -1,6 +1,8 @@
 package JAVA2;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main2 {
     public static void main(String[] args) {
@@ -8,7 +10,7 @@ public class Main2 {
         //ZADANIE1
         Scanner skanerMoj = new Scanner(System.in);
         System.out.println("ZADANIE 1:");
-        System.out.println("Podaj liczbę całkowitą");
+        System.out.println("Podaj liczbę całkowitą - sprawdzę jej parzystość");
         int liczba = skanerMoj.nextInt();
            if (liczba % 2 == 0) {
            System.out.println("twoja liczba jest parzysta");
@@ -19,7 +21,7 @@ public class Main2 {
 
         //ZADANIE2
         System.out.println("ZADANIE 2:");
-        System.out.println("Podaj liczbę całkowitą");
+        System.out.println("Podaj liczbę całkowitą - sprawdzę jej parzystość");
         int liczba2 = skanerMoj.nextInt();
         int a = liczba2 % 2;
         switch (a) {
@@ -30,27 +32,41 @@ public class Main2 {
                 System.out.println("twoja liczba jest parzysta");
                         }
 
+        //ZADANIE3
+        System.out.println("ZADANIE 3:");
+        int[] table = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            Random rand = new Random();
+            table[i] = rand.nextInt();
+        }
+        System.out.println("Oto wygenerowana tablica:");
+        for (int item : table) {
+            System.out.println(item);
+        }
+
+
+
         //ZADANIE4
         System.out.println("ZADANIE 4:");
 
         int c =0;
         int d = 0;
-        do { System.out.println("Podaj liczbę całkowitą");
+        do { System.out.println("Podaj liczbę całkowitą, jaką chcesz dodać. Uwaga: jak podasz 0 to program sie zatrzyma");
             int liczba4 = skanerMoj.nextInt();
             c = liczba4 ;
             d = d + c;
             }
         while (c !=0);
 
-
         if (c == 0){
-            System.out.println("suma twoich liczb to: ");
+            System.out.println("suma twoich liczb, zanim podałeś 0, to: ");
             System.out.println(d);}
 
 
         //ZADANIE5
         System.out.println("ZADANIE 5:");
-        System.out.println("Podaj liczbę całkowitą");
+        System.out.println("Podaj liczbę całkowitą - sprawdzę, czy należy do przedziału <1,10>");
         int liczba3 = skanerMoj.nextInt();
         if (liczba3 >= 1 && liczba3 <=10){
             System.out.println("twoja liczba należy do przedziału <1,10>");}
@@ -62,7 +78,7 @@ public class Main2 {
          System.out.println("ZADANIE 6:");
 
         int b =0;
-            do { System.out.println("Podaj liczbę całkowitą");
+            do { System.out.println("Podaj liczbę całkowitą. Uwaga: jak podasz 0 lub 5, to program się zatrzyma");
                 int liczba4 = skanerMoj.nextInt();
                 b = liczba4 ; }
         while (b !=0 && b !=5);
@@ -74,7 +90,7 @@ public class Main2 {
 
         //ZADANIE7
         System.out.println("ZADANIE 7:");
-        System.out.println("Podaj liczbę całkowitą");
+        System.out.println("Podaj liczbę całkowitą - sprawdzę jej parzystość");
         int liczba5 = skanerMoj.nextInt();
         while (liczba5 %2 == 0){
             System.out.println("twoja liczba jest parzysta");
