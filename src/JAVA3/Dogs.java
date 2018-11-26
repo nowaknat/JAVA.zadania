ceada53c1a0d6d78cec8902f826ea693a3476383
nowaknat <natalia.nowak@tieto.com>
2018-11-26 10:14:00 +0100

    dodałam zadania 1-5

--- src/JAVA3/Dogs.java
+++ src/JAVA3/Dogs.java
@@ -1,14 +1,34 @@
 package JAVA3;
 
 public class Dogs {
-    String rasa;
+    String rasa; //POLA OBIEKTÓW - zad 1
     int wiek;
     String imię;
     String kolor;
 
+    public Dogs() {
+    }
 
+    public Dogs(String rasa, int wiek, String imię, String kolor) { //konstruktor - zad 2
+        this.rasa = rasa;
+        this.kolor = kolor;
+        this.wiek = wiek;
+        this.imię = imię;
+    }
 
+    public void info() { //zad 3
+        System.out.println(imię + ", " + wiek + ", " + kolor + ", " + rasa);
+    }
 
+    public int wiekpsa() {
+        return wiek;
+    }
 
-
+    public int aktualizacja(int wiekNowy) {
+        if (wiekNowy > 0) {
+            wiek = wiekNowy;
+        }
+        return wiek;
+    }
 }
+
--- src/JAVA3/Main3.java
+++ src/JAVA3/Main3.java
@@ -6,12 +6,35 @@ public class Main3 {
     public static void main(String[] args) {
 
         //ZADANIE 1
-        System.out.println("Oto zadanie 1:");
+        System.out.println("Zadanie 1:");
         Dogs dog1 = new Dogs();
-        dog1.imię="burek";
-        dog1.wiek=3;
-        dog1.rasa="pudel";
-        dog1.kolor="czarny";
-        System.out.println(dog1.imię+", "+dog1.kolor);
+        dog1.imię = "burek";
+        dog1.wiek = 3;
+        dog1.rasa = "pudel";
+        dog1.kolor = "czarny";
+        System.out.println(dog1.imię + ", " + dog1.kolor + ", " + dog1.wiek + " lata, " + dog1.rasa);
+
+        //ZADANIE 2
+        System.out.println("Zadanie 2:");
+        Dogs dog2 = new Dogs("Owczarek", 12, "Lola", "zielony");
+        System.out.println(dog2.imię + dog2.rasa + dog2.wiek + dog2.kolor);
+
+
+        //ZADANIE 3 i 5
+        System.out.println("Zadanie 3 i 5:");
+        dog2.info();
+        System.out.println(dog2.wiekpsa());
+        dog2.aktualizacja(5);
+        System.out.println(dog2.wiekpsa());
+
+
+        //ZADANIE 4
+        System.out.println("Zadanie 4:");
+        Dogs[] array1 = {dog1,dog2};
+        for(Dogs i : array1) { //działam na obiektach typu Dogs, dla każdego argumentu i z tablicy array1
+            i.info();
+        }
+        }
+
+
     }
-}
